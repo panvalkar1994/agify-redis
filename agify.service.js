521ace0ccb1e7a6d5ad99f1e176da395370be2a9
vaibhav <vpanvalkar@outlook.com>
2021-12-25 17:27:42 +0530

    get and set name using redis

--- agify.service.js
+++ agify.service.js
@@ -1,14 +1,35 @@
 import axios from "axios";
 import { Constants } from "./constants.js";
 
-export async function getAgePredictionByName(name) {
-  const response = await axios.get(Constants.BaseUrl, {
+export async function getAgePredictionByName(name, redisClient) {
+  let cachedValue = getDataFromCacheByName(redisClient, name);
+  if (cachedValue) {
+    return cachedValue;
+  }
+  const response = await axios.get(Constants.AgifyBaseUrl, {
     params: {
       name: name,
     },
   });
   if (response.status == 200) {
+    await setDataToCacheAgainstName(redisClient, name, response.data);
     return response.data;
   }
   return {};
+}
+
+async function getDataFromCacheByName(redisClient, name) {
+  if (redisClient && name) {
+    const cachedValue = await redisClient.get(name);
+    if (cachedValue) {
+      return JSON.parse(cachedValue);
+    }
+  }
+  return null;
+}
+
+async function setDataToCacheAgainstName(redisClient, name, data) {
+  if (redisClient && name && Object.keys(data).length) {
+    await redisClient.set(name, JSON.stringify(data));
+  }
 }
