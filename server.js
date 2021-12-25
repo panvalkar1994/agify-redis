25b0f967ef8960af7d875fce6731b8964b1cf3a8
vaibhav <vpanvalkar@outlook.com>
2021-12-25 17:27:23 +0530

    redis client init

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-import express from 'express';
-import { getAgePredictionByName } from './agify.service.js';
+import express from "express";
+import { getAgePredictionByName } from "./agify.service.js";
+import { createClient } from "redis";
+import { Constants } from "./constants.js";
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-app.post('/name', async (req, res)=>{
-    const name = req.body.name || "";
-    const data = await getAgePredictionByName(name)
-    res.json(data)
+const client = createClient();
+client
+  .connect()
+  .then((x) => {
+    console.log("connected to redis...", x);
+  })
+  .catch((e) => {
+    console.log("connection failed", e);
+  });
+
+app.post("/name", async (req, res) => {
+  const name = req.body.name || "";
+  const data = await getAgePredictionByName(name, client);
+  res.json(data);
 });
 
-app.post('/names', (req, res)=>{
-    const names = req.body.names || [];
-    console.log('names', names)
-    res.send(`You sent ${names.length} names`);
-})
+app.post("/names", (req, res) => {
+  const names = req.body.names || [];
+  console.log("names", names);
+  res.send(`You sent ${names.length} names`);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`app started listening on http://localhost:${PORT} ...`);
-})
+app.listen(PORT, () => {
+  console.log(`app started listening on http://localhost:${PORT} ...`);
+});
